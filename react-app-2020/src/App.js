6afe61c8f2ac053eb81f77f2eb6400269ebf8e0e
Zeeshan Ali <zeeshan2193@gmail.com>
2020-06-07 02:57:52 +0500

    Update App.js

--- react-app-2020/src/App.js
+++ react-app-2020/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn More About React 
+          
         </a>
         <p>Here We are going to learn react</p>
       </header>
